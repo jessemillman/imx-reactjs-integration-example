11cc3b5354e2c649f3de43eb04aa431266940548
imrankhan <imran.khan@accubits.com>
2022-07-07 15:40:13 +0530

    popup added

--- src/Components/AssetDetails/AssetDetails.tsx
+++ src/Components/AssetDetails/AssetDetails.tsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { Link, ImmutableXClient, ImmutableMethodResults, MintableERC721TokenType } from '@imtbl/imx-sdk';
+import { useState } from "react";
 import './AssetDetails.css'
+import CommonPopup from '../../Components/Popup/CommonPopup';
+
 interface AsserProps {
     client: ImmutableXClient,
     link: Link,
@@ -8,52 +11,66 @@ interface AsserProps {
     details?: any;
 }
 type LocationState = {
-   from:{pathName:string,
-    id:any
-   }
-  }
+    from: {
+        pathName: string,
+        id: any
+    }
+}
+
 const AssetDetails = ({ client, link, wallet, details }: AsserProps) => {
 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-    const clickFn = ()=>{
-        
+    const clickFn = () => {
+
     }
-     const {pageName, id} = useParams();
+    const { pageName, id } = useParams();
+
+
     return (
-        console.log(details,pageName,id),
-        <div className='mint-div'>
-            <div className='inline-mint'>
-                <div className="main-asset">
-                    <div className="img-section">
-                        <img src={pageName==="listing"?details['sell']['data']['properties']['image_url']:"Inventory-section"} alt='test' />
-                    </div>
-
-                    <div className="detail-section">
-                        <div className='sub-container'>
-                            <img className='avatar-img'
-                                src={pageName==="listing"?details['sell']['data']['properties']['collection']['icon_url']:"Inventory-section"} alt="" />
-                            <span className='text-spn'>{pageName==="listing"?details['sell']['data']['properties']['collection']['name']:'Inventory-section'}</span>
-                        </div>
-                        <div>
-                            <h1>{pageName==="listing"?details['sell']['data']['properties']['name']:'Inventory-section'}</h1>
-                            <span className='own-label'>Owned by </span><span className='eth-amount'>{pageName==="listing"?`${details?.user?.slice(0, 8)}...${details?.user?.slice(-4)}`:`Inventory-section`}</span>
+        <>
+            <div className='mint-div'>
+                <div className='inline-mint'>
+                    <div className="main-asset">
+                        <div className="img-section">
+                            <img src={pageName === "listing" ? details['sell']['data']['properties']['image_url'] : details.image_url} alt='test' />
                         </div>
 
-                        <div className='btn-section'>
+                        <div className="detail-section">
+                            <div className='sub-container'>
+                                <img className='avatar-img'
+                                    src={pageName === "listing" ? details['sell']['data']['properties']['collection']['icon_url'] : details['collection'].icon_url} alt="" />
+                                <span className='text-spn'>{pageName === "listing" ? details['sell']['data']['properties']['collection']['name'] : details['collection'].name}</span>
+                            </div>
                             <div>
-                                <i className='fab fa-ethereum'></i><span>0.14105($165.28 USD)</span>
+                                <h1>{pageName === "listing" ? details['sell']['data']['properties']['name'] : details.name}</h1>
+                                <span className='own-label'>Owned by </span><span className='eth-amount'>{`${details?.user?.slice(0, 8)}...${details?.user?.slice(-4)}`}</span>
                             </div>
 
+                            <div className='btn-section'>
+                                <div className="amount">
+                                    <i className='fab fa-ethereum'></i> <span> 0.14105($165.28 USD)</span>
+                                </div>
 
-                            {pageName==="listing"?<button onClick={ ()=>clickFn()}className="invent-btns btn-position">Buy</button>:<div>Inventory-section</div>}
 
+                                {pageName === "listing" ? <button onClick={() => clickFn()} className="invent-btns btn-position">Buy</button> :
+
+                                    <div className="btn-style ">
+                                        <button onClick={() => setShow(true)} className="invent-btns btn-position">transfer</button>
+                                        <button onClick={() => setShow(true)} className="invent-btns btn-position">Sale</button>
+                                    </div>}
+
+                            </div>
                         </div>
+
                     </div>
 
                 </div>
-
             </div>
-        </div>
+            <CommonPopup show={show} handleClose = {handleClose} />
+        </>
 
     )
 }
