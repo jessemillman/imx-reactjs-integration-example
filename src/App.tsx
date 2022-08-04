a4327ab3f63492b71b88907b7d7cefcbbeff4edd
Jesse Millman <jesse@jessemillman.com.au>
2022-08-04 23:59:01 +0800

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,81 @@
-
-import './App.css';
-import { Link, ImmutableXClient, SyncStateEventPayload, ProviderPreference } from '@imtbl/imx-sdk';
-import { getConfig, BalancesApi } from '@imtbl/core-sdk';
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Marketplace from './Marketplace';
-import Inventory from './Inventory';
-import Bridging from './Bridging';
-import Sidebar from './Components/Sidebar/Sidebar';
-import getRoutes from './Router';
-import ConnectWalletSection from './Components/ConnectWalletSection/ConnectWalletSection';
-import State from './State';
-import { ethers } from 'ethers';
-require('dotenv').config();
-
-
+import "./App.css";
+import {
+  Link,
+  ImmutableXClient,
+  SyncStateEventPayload,
+  ProviderPreference,
+} from "@imtbl/imx-sdk";
+import { getConfig, BalancesApi } from "@imtbl/core-sdk";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Marketplace from "./Marketplace";
+import Inventory from "./Inventory";
+import Bridging from "./Bridging";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import getRoutes from "./Router";
+import ConnectWalletSection from "./Components/ConnectWalletSection/ConnectWalletSection";
+import State from "./State";
+import { ethers } from "ethers";
+require("dotenv").config();
 
 const App = () => {
-  const networkType: any = process.env.REACT_APP_NETWORK_TYPE
+  const networkType: any = process.env.REACT_APP_NETWORK_TYPE;
   const config = getConfig(networkType);
   const balanceApi = new BalancesApi(config.api);
-const [selectedDetails,setSelectedDetails]=useState<any>(Object)
+  const [selectedDetails, setSelectedDetails] = useState<any>(Object);
   // initialise Immutable X Link SDK
-  const link = new Link(process.env.REACT_APP_ROPSTEN_LINK_URL)
+  const link = new Link(process.env.REACT_APP_ROPSTEN_LINK_URL);
 
   // general
-  const [tab, setTab] = useState('marketplace');
-  const [wallet, setWallet] = useState('undefined');
+  const [tab, setTab] = useState("marketplace");
+  const [wallet, setWallet] = useState("undefined");
   const [balance, setBalance] = useState<any>(Object);
   const [client, setClient] = useState<ImmutableXClient>(Object);
-  const [sidebar, setSidebar] = useState(true)
-  const [selectedOrderId, setSelectedOrderId] = useState('');
+  const [sidebar, setSidebar] = useState(true);
+  const [selectedOrderId, setSelectedOrderId] = useState("");
   const [statemaintain, setStatemaintain] = useState<any>([]);
   // const [assetdetail, setAssets] = useState();
   const stateArr: any = [];
   useEffect(() => {
     // navigate('/listing')
-    buildIMX()
-  }, [])
+    buildIMX();
+  }, []);
 
-  const setAssets=(data:any)=>{
-    localStorage.setItem("assetdetail",JSON.stringify(data))
-  }
+  const setAssets = (data: any) => {
+    localStorage.setItem("assetdetail", JSON.stringify(data));
+  };
   useEffect(() => {
     // setStatemaintain(stateArr)
-  }, [statemaintain])
+  }, [statemaintain]);
 
-  useEffect(()=>{
-setSelectedDetails(JSON.parse(localStorage.getItem("assetdetail")||"{}"))
-  },[localStorage.getItem("assetdetail")])
+  useEffect(() => {
+    setSelectedDetails(JSON.parse(localStorage.getItem("assetdetail") || "{}"));
+  }, [localStorage.getItem("assetdetail")]);
 
   // initialise an Immutable X Client to interact with apis more easily
   async function buildIMX() {
-    const publicApiUrl: string = process.env.REACT_APP_ROPSTEN_ENV_URL ?? '';
-    setClient(await ImmutableXClient.build({ publicApiUrl }))
+    const publicApiUrl: string =  "https://api.ropsten.x.immutable.com/v1";
+    setClient(await ImmutableXClient.build({ publicApiUrl }));
   }
 
   // register and/or setup a user
   async function linkSetup(): Promise<void> {
     // console.log('APP COMPONENT')
-    const res = await link.setup({ providerPreference: ProviderPreference.NONE })
-    // const res = await link.setup({})
-localStorage.setItem("address",res.address)
-    setWallet(res.address)
-    const balnaceresponce: any = await balanceApi.getBalance({ owner: res.address, address: 'eth' })
-    balnaceresponce['data']['balance'] = ethers.utils.formatUnits(balnaceresponce['data']['balance'], 18);
-    localStorage.setItem("balance",JSON.stringify(balnaceresponce['data']))
-    setBalance(balnaceresponce['data'])
+    const res = await link.setup({});
+    localStorage.setItem("address", res.address);
+    setWallet(res.address);
+    const balnaceresponce: any = await balanceApi.getBalance({
+      owner: res.address,
+      address: "eth",
+    });
+    balnaceresponce["data"]["balance"] = ethers.utils.formatUnits(
+      balnaceresponce["data"]["balance"],
+      18
+    );
+    localStorage.setItem("balance", JSON.stringify(balnaceresponce["data"]));
+    setBalance(balnaceresponce["data"]);
 
-    
-
-    const syncStateObservable = await link.syncState({})
+    const syncStateObservable = await link.syncState({});
     syncStateObservable.subscribe((syncStateEvent: any) => {
       // let isavailable = stateArr.filter((singlestate: any) => {
       //   return singlestate.connectedWalletAddress.toLowerCase() == syncStateEvent['connectedWalletAddress'].toLowerCase()
@@ -79,104 +84,143 @@ localStorage.setItem("address",res.address)
       // setTimeout(()=>{
       // stateArr.push(syncStateEvent)
       // console.log('inser', stateArr)
-      
-      localStorage.setItem("stateDetails",JSON.stringify(syncStateEvent))
-      setStatemaintain(syncStateEvent)
+
+      localStorage.setItem("stateDetails", JSON.stringify(syncStateEvent));
+      setStatemaintain(syncStateEvent);
       // },100)
 
       // }
-    })
-
-  };
+    });
+  }
   const disconnectWalletHandler = () => {
-    setWallet('undefined')
+    setWallet("undefined");
     localStorage.removeItem("address");
     localStorage.removeItem("balance");
     localStorage.removeItem("stateDetails");
     localStorage.removeItem("assetdetail");
     localStorage.removeItem("Result");
     localStorage.removeItem("signinMessage");
-  }
+  };
 
   function handleTabs() {
     if (client.address) {
       switch (tab) {
-        case 'inventory':
-          if (wallet === 'undefined') return <div>Connect wallet</div>
-          return <Inventory
-            client={client}
-            link={link}
-            wallet={wallet}
-          />
-        case 'bridging':
-          if (wallet === 'undefined') return <div>Connect wallet</div>
-          return <Bridging
-            client={client}
-            link={link}
-            wallet={wallet}
-          />
+        case "inventory":
+          if (wallet === "undefined") return <div>Connect wallet</div>;
+          return <Inventory client={client} link={link} wallet={wallet} />;
+        case "bridging":
+          if (wallet === "undefined") return <div>Connect wallet</div>;
+          return <Bridging client={client} link={link} wallet={wallet} />;
         default:
-          return <Marketplace
-            client={client}
-            link={link}
-            selectedOrderId={selectedOrderId}
-          />
+          return (
+            <Marketplace
+              client={client}
+              link={link}
+              selectedOrderId={selectedOrderId}
+            />
+          );
       }
     }
-    return null
+    return null;
   }
 
   const setSidebarHandler = (value: boolean) => {
-    return setSidebar(value)
+    return setSidebar(value);
   };
 
   const redirect = (btnName: any) => {
-    if (btnName == 'Start') {
-      window.open('https://docs.x.immutable.com/', '_blank', 'noopener,noreferrer');
+    if (btnName == "Start") {
+      window.open(
+        "https://docs.x.immutable.com/",
+        "_blank",
+        "noopener,noreferrer"
+      );
       // window.location.href = "";
-    } else if (btnName == 'Contact') {
-      window.open('https://www.immutable.com/contact', '_blank', 'noopener,noreferrer');
+    } else if (btnName == "Contact") {
+      window.open(
+        "https://www.immutable.com/contact",
+        "_blank",
+        "noopener,noreferrer"
+      );
       // window.location.href = "";
     }
+  };
 
-  }
-
-  const getSelectedDetails=()=>{
-   setSelectedDetails(JSON.parse(localStorage.getItem("assetdetail")||"{}"))
-   setBalance(JSON.parse(localStorage.getItem("balance")||"{}"))
-   setWallet(localStorage.getItem("address")||'undefined')
-   setStatemaintain(JSON.parse(localStorage.getItem("stateDetails")||'{}'))
-   
-  }
+  const getSelectedDetails = () => {
+    setSelectedDetails(JSON.parse(localStorage.getItem("assetdetail") || "{}"));
+    setBalance(JSON.parse(localStorage.getItem("balance") || "{}"));
+    setWallet(localStorage.getItem("address") || "undefined");
+    setStatemaintain(JSON.parse(localStorage.getItem("stateDetails") || "{}"));
+  };
 
   return (
     <div className="App">
-      <div className='sidebar'>
-        {sidebar && <Sidebar setbalanceValue={balance} address={wallet} sigin={linkSetup} setSideHandler={setSidebarHandler} disconnectWalletHandler={disconnectWalletHandler} getSelectedDetails={getSelectedDetails} />}
-        <div className='inner-section'>
-          <div className='header-title'>
-            {!sidebar && <div className='hamburger-div'>
-              <svg aria-hidden="true" className='pointer' onClick={() => setSidebarHandler(true)} role="img" width="3em" height="3em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5" /></svg>
-            </div>}
-            <div className='header-text-div'>
-              <h1>Powering the next generation of web3 games
-              </h1>
-              <div className='header-btn'>
-                <button className='button1' onClick={(e) => redirect('Start')} type="button">Start Building</button>
-                <button className='button2' onClick={(e) => redirect('Contact')} type="button">Contact Us</button>
+      <div className="sidebar">
+        {sidebar && (
+          <Sidebar
+            setbalanceValue={balance}
+            address={wallet}
+            sigin={linkSetup}
+            setSideHandler={setSidebarHandler}
+            disconnectWalletHandler={disconnectWalletHandler}
+            getSelectedDetails={getSelectedDetails}
+          />
+        )}
+        <div className="inner-section">
+          <div className="header-title">
+            {!sidebar && (
+              <div className="hamburger-div">
+                <svg
+                  aria-hidden="true"
+                  className="pointer"
+                  onClick={() => setSidebarHandler(true)}
+                  role="img"
+                  width="3em"
+                  height="3em"
+                  preserveAspectRatio="xMidYMid meet"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="1.5"
+                    d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"
+                  />
+                </svg>
+              </div>
+            )}
+            <div className="header-text-div">
+              <h1>Powering the next generation of web3 games</h1>
+              <div className="header-btn">
+                <button
+                  className="button1"
+                  onClick={(e) => redirect("Start")}
+                  type="button"
+                >
+                  Start Building
+                </button>
+                <button
+                  className="button2"
+                  onClick={(e) => redirect("Contact")}
+                  type="button"
+                >
+                  Contact Us
+                </button>
               </div>
             </div>
-            <div className='logoframe'>
-              <iframe src={'https://player.vimeo.com/video/720459459?h=8ce82285ae&autoplay=1&muted=1&loop=1&title=0&byline=0&portrait=0&background=1:'}></iframe>
+            <div className="logoframe">
+              <iframe
+                src={
+                  "https://player.vimeo.com/video/720459459?h=8ce82285ae&autoplay=1&muted=1&loop=1&title=0&byline=0&portrait=0&background=1:"
+                }
+              ></iframe>
             </div>
           </div>
-          <div className='sub-content'>
-
-
+          <div className="sub-content">
             <Routes>
-
               {getRoutes().map((item, key) => (
-
                 // item.path == '/state' ? (
                 //   < Route
                 //     path='/state'
@@ -187,21 +231,31 @@ localStorage.setItem("address",res.address)
                 //   >
                 //   </Route>
                 // ) : (
-                < Route
+                <Route
                   path={item.path}
                   key={key}
-                  element={(wallet && wallet !== "undefined") || item.skip ? <item.element client={client} selectedOrderId={selectedOrderId}
-                    setSelectedOrderId={setSelectedOrderId}
-                    link={link}
-                    wallet={localStorage.getItem("address")} sigin={linkSetup} stateDetails={[statemaintain]} details={selectedDetails} getSelectedDetails={getSelectedDetails} setAssets={setAssets} /> : <ConnectWalletSection />}
-                >
-                </Route>
+                  element={
+                    (wallet && wallet !== "undefined") || item.skip ? (
+                      <item.element
+                        client={client}
+                        selectedOrderId={selectedOrderId}
+                        setSelectedOrderId={setSelectedOrderId}
+                        link={link}
+                        wallet={localStorage.getItem("address")}
+                        sigin={linkSetup}
+                        stateDetails={[statemaintain]}
+                        details={selectedDetails}
+                        getSelectedDetails={getSelectedDetails}
+                        setAssets={setAssets}
+                      />
+                    ) : (
+                      <ConnectWalletSection />
+                    )
+                  }
+                ></Route>
                 // )
-
               ))}
             </Routes>
-
-
 
             {/* {wallet === 'undefined' 
             return(
@@ -209,14 +263,9 @@ localStorage.setItem("address",res.address)
             )
               
             } */}
-
           </div>
         </div>
       </div>
-
-
-
-
 
       {/* <button onClick={linkSetup}>Setup</button>
       <div>
@@ -230,8 +279,8 @@ localStorage.setItem("address",res.address)
       <button onClick={() => setTab('bridging')}>Deposit and withdrawal</button>
       <br/><br/><br/>
       {handleTabs()} */}
-    </div >
+    </div>
   );
-}
+};
 
 export default App;
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,139 +1,193 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom"
-import immuLogo from '../../assets/logo.svg';
+import { useNavigate, useLocation } from "react-router-dom";
+import immuLogo from "../../assets/logo.svg";
 import useOnclickOutside from "react-cool-onclickoutside";
 
-import './Sidebar.css';
+import "./Sidebar.css";
 interface Props {
-    setbalanceValue: any,
-    address: any,
-    sigin: () => any;
-    setSideHandler: (params: boolean) => any;
-    disconnectWalletHandler: () => any;
-    getSelectedDetails:any
+  setbalanceValue: any;
+  address: any;
+  sigin: () => any;
+  setSideHandler: (params: boolean) => any;
+  disconnectWalletHandler: () => any;
+  getSelectedDetails: any;
 }
 
-const Sidebar = ({ setbalanceValue, address, sigin, setSideHandler, disconnectWalletHandler,getSelectedDetails }: Props) => {
-    const [sidebarTab, setSidebarTab] = useState("listing");
-    const [addressDropdown, setAddressDropdown] = useState(false);
+const Sidebar = ({
+  setbalanceValue,
+  address,
+  sigin,
+  setSideHandler,
+  disconnectWalletHandler,
+  getSelectedDetails,
+}: Props) => {
+  const [sidebarTab, setSidebarTab] = useState("listing");
+  const [addressDropdown, setAddressDropdown] = useState(false);
 
-    let navigate = useNavigate()
-    const location = useLocation();
-    // const balance = ethers.utils.parseEther(setbalanceValue?.balance);
-    useEffect(() => {
-        setSidebarTab(location?.pathname?.split("/")[1])
-    }, [location?.pathname])
+  let navigate = useNavigate();
+  const location = useLocation();
+  // const balance = ethers.utils.parseEther(setbalanceValue?.balance);
+  useEffect(() => {
+    setSidebarTab(location?.pathname?.split("/")[1]);
+  }, [location?.pathname]);
 
-useEffect(()=>{
-    getSelectedDetails()
-},[])
-    const dropDownuterClick = useOnclickOutside(() => {
-        setAddressDropdown(false);
-    });
+  useEffect(() => {
+    getSelectedDetails();
+  }, []);
+  const dropDownuterClick = useOnclickOutside(() => {
+    setAddressDropdown(false);
+  });
 
+  const sidebarConfig = [
+    {
+      LabelName: "Listing",
+      Link: "/listing",
+      icon: "fa fa-briefcase",
+    },
+    {
+      LabelName: "Inventory",
+      Link: "/inventory",
+      icon: "fa fa-user",
+    },
+    {
+      LabelName: "Mint",
+      Link: "/minting",
+      icon: "fa fa-plus",
+    },
+    {
+      LabelName: "Signing",
+      Link: "/signing",
+      icon: "far fa-edit",
+    },
+    {
+      LabelName: "State",
+      Link: "/state",
+      icon: "fa fa-hdd-o",
+    },
+    {
+      LabelName: "Deposit",
+      Link: "/deposit",
+      icon: "fa fa-hand-o-up",
+    },
+    {
+      LabelName: "Withdrawal",
+      Link: "/withdrawal",
+      icon: "fa fa-hand-o-down",
+    },
+    {
+      LabelName: "Settings",
+      Link: "/settings",
+      icon: "fa fa-gear",
+    },
+  ];
 
-
-
-    const sidebarConfig = [
-        {
-            LabelName: 'Listing',
-            Link: '/listing',
-            icon: "fa fa-briefcase"
-        },
-        {
-            LabelName: 'Inventory',
-            Link: '/inventory',
-            icon: "fa fa-user"
-        },
-        {
-            LabelName: 'Mint',
-            Link: '/minting',
-            icon: "fa fa-plus"
-        },
-        {
-            LabelName: 'Signing',
-            Link: '/signing',
-            icon: "far fa-edit"
-        }, {
-            LabelName: 'State',
-            Link: '/state',
-            icon: "fa fa-hdd-o"
-        }, {
-            LabelName: 'Deposit',
-            Link: '/deposit',
-            icon: "fa fa-hand-o-up"
-        },
-        {
-            LabelName: 'Withdrawal',
-            Link: '/withdrawal',
-            icon: "fa fa-hand-o-down"
-        }, {
-            LabelName: 'Settings',
-            Link: '/settings',
-            icon: 'fa fa-gear'
-        }
-
-    ]
-
-
-
-
-    return (
-        <div className="main-sidebar">
-            <div className='hamburger-div'>
-                <svg aria-hidden="true" className='pointer'
-                    onClick={() => setSideHandler(false)} role="img" width="2em" height="2em"
-                    preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16">
-                    <path fill="none" stroke="currentColor" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-width="1.5" d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5" /></svg>
+  return (
+    <div className="main-sidebar">
+      <div className="hamburger-div">
+        <img src={immuLogo} alt="immutableX" />
+        <svg
+          aria-hidden="true"
+          className="pointer"
+          onClick={() => setSideHandler(false)}
+          role="img"
+          width="2em"
+          height="2em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"
+          />
+        </svg>
+      </div>
+      <div className="logo">
+        <div className="signin-btn" ref={dropDownuterClick}>
+          <button
+            className={
+              address && address !== "undefined"
+                ? "connect-btn address-text"
+                : "connect-btn"
+            }
+            onClick={
+              address && address !== "undefined"
+                ? () => setAddressDropdown(!addressDropdown)
+                : sigin
+            }
+            type="button"
+          >
+            {address && address !== "undefined" ? (
+              <>
+                {address?.slice(0, 6)}...{address?.slice(-4)}
+                <i className="fa fa-angle-down"></i>
+              </>
+            ) : (
+              `Connect Wallet`
+            )}
+          </button>
+          {addressDropdown && (
+            <div className="addressDropdown">
+              <div
+                className="disconnect-wallet pointer"
+                onClick={() => {
+                  disconnectWalletHandler();
+                  navigate("/listing");
+                  setAddressDropdown(false);
+                }}
+              >
+                Disconnect Wallet
+              </div>
             </div>
-            <div className="logo">
-                <img src={immuLogo} alt="immutableX" />
-                <div className="sigin-btn" ref={dropDownuterClick}>
-                    <button className={(address && address !== "undefined") ? "connect-btn address-text" : "connect-btn"}
-                        onClick={(address && address !== "undefined") ? () => setAddressDropdown(!addressDropdown) : sigin}
-                        type="button">
-                        {(address && address !== "undefined") ? <>{address?.slice(0, 6)}...{address?.slice(-4)}<i className="fa fa-angle-down"></i></>
-                            : `Connect Wallet`}
-                    </button>
-                    {addressDropdown && <div className="addressDropdown">
-                        <div className="disconnect-wallet pointer" onClick={() => {
-                            disconnectWalletHandler()
-                            navigate('/listing')
-                            setAddressDropdown(false)
-                        }}>
-                            Disconnect Wallet
-                        </div>
-                    </div>}
-                </div>
+          )}
+        </div>
+      </div>
+      <ul className="sidebar-options">
+        {sidebarConfig.map((menu, ind) => {
+          return (
+            <li
+              key={ind}
+              className={`${
+                menu.Link.includes(sidebarTab) ? "active-tab" : "tab"
+              }   pointer`}
+              // ${(!address || address === "undefined") && "active-tab"}
+              onClick={() => {
+                navigate(menu.Link);
+              }}
+            >
+              {/* (address && address !== "undefined") && */}
+              <i className={menu.icon} aria-hidden="true"></i>
+              <span>{menu.LabelName}</span>
+            </li>
+          );
+        })}
+      </ul>
+      {address && address !== "undefined" && (
+        <div className="balance-div">
+          <h5>
+            Your Balance <br></br>{" "}
+            <div className="inline-text">
+              {setbalanceValue?.balance?.toString()}
             </div>
-            <ul className="sidebar-options">
-                {
-                    sidebarConfig.map((menu, ind) => {
-                        return (
-                            <li key={ind} className={`${menu.Link.includes(sidebarTab) ? "active-tab" : "tab"}   pointer`}
-                                // ${(!address || address === "undefined") && "active-tab"}
-                                onClick={() => { navigate(menu.Link) }}>
-                                {/* (address && address !== "undefined") && */}
-                                <i className={menu.icon} aria-hidden="true"></i>
-                                <span>{menu.LabelName}</span>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-            {(address && address !== "undefined") && <div className="balance-div">
-                <h5>Your Balance <br></br> <div className="inline-text">{setbalanceValue?.balance?.toString()}</div></h5>
-                <div className="eth-div"><span className="eth-icon"><i className='fab fa-ethereum'></i></span>ETH</div>
-                <button className="bal-btn" type="button">
-                    <i className="fa fa-plus-circle"></i>
-                    <span className="bal-btn-text">Top Up Balance</span>
-                    <i className="fa fa-angle-right"></i>
-                </button>
-            </div>}
-        </div >
-    )
+          </h5>
+          <div className="eth-div">
+            <span className="eth-icon">
+              <i className="fab fa-ethereum"></i>
+            </span>
+            ETH
+          </div>
+          <button className="bal-btn" type="button">
+            <i className="fa fa-plus-circle"></i>
+            <span className="bal-btn-text">Top Up Balance</span>
+            <i className="fa fa-angle-right"></i>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-}
-
-export default Sidebar
+export default Sidebar;
